0703c59281b6d27d58cf52e54d0d9a199385fdb4
kinouchi1000 <drums.radwimps203@gmail.com>
2022-01-09 05:35:06 +0900

    Merge: Merge all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,13 @@ import type { NextPage } from 'next'
 import Link from 'next/link'
 import { client } from './api/client'
 import { Layout } from './layout'
-<<<<<<< HEAD
+
 import styles from '../styles/Home.module.css'
 import type { SubCategories, MainCategories, SubCategory, Category } from '../interface/Category'
 import type { Article, Articles } from '../interface/Article'
-=======
->>>>>>> f7fb7cbcf1bcb81cb9cb3ab6ef270850826da7ee
 
 export const Home: NextPage = ({ blog }: { children?: ReactNode; blog?: Article[] }) => {
   return (
-<<<<<<< HEAD
     <Layout>
       <h1 className={styles.title}>Welcome to my blog!</h1>
       <div>
@@ -28,25 +25,6 @@ export const Home: NextPage = ({ blog }: { children?: ReactNode; blog?: Article[
         </ul>
       </div>
     </Layout>
-=======
-    <CategoriesContext.Provider value={categories}>
-      <Layout>
-        <h1 className='m-0 text-6xl leading-tight'>Welcome to my blog!</h1>
-        <div>
-          <ul>
-            {blog &&
-              blog.map((article) => (
-                <li key={article.id}>
-                  <Link href={`/blog/${article.id}`}>
-                    <a>{article.title}</a>
-                  </Link>
-                </li>
-              ))}
-          </ul>
-        </div>
-      </Layout>
-    </CategoriesContext.Provider>
->>>>>>> f7fb7cbcf1bcb81cb9cb3ab6ef270850826da7ee
   )
 }
 
