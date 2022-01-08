8c3a871c6693ba717da8697a0553883d28238737
kinouchi1000 <drums.radwimps203@gmail.com>
2022-01-09 05:18:03 +0900

    commit 漏れ

--- src/pages/blog/[id].tsx
+++ src/pages/blog/[id].tsx
@@ -1,33 +1,20 @@
 // pages/blog/[id].js
-import { ReactNode, createContext } from 'react'
-import type { NextPage } from 'next'
+import { ReactNode } from 'react'
 import { client } from '../api/client'
 import { Layout } from '../layout'
-import type { SubCategories, MainCategories, SubCategory, Category } from '../../interface/Category'
 import type { Article, Articles, Content } from '../../interface/Article'
 
-export const CategoriesContext = createContext<Category[] | undefined>(undefined)
-
-export default function BlogId({
-  blog,
-  categories,
-}: {
-  children?: ReactNode
-  blog?: Article
-  categories?: Category[]
-}) {
+export default function BlogId({ blog }: { children?: ReactNode; blog?: Article }) {
   return (
-    <CategoriesContext.Provider value={categories}>
-      <Layout>
-        <h1>{blog && blog.title}</h1>
-        <p>{blog && blog.publishedAt}</p>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `${blog && blog.body}`,
-          }}
-        />
-      </Layout>
-    </CategoriesContext.Provider>
+    <Layout>
+      <h1>{blog && blog.title}</h1>
+      <p>{blog && blog.publishedAt}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog && blog.body}`,
+        }}
+      />
+    </Layout>
   )
 }
 
@@ -43,23 +30,10 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context: Content) => {
   const id = context.params.id
   const blog: Article = await client.get({ endpoint: 'blog', contentId: id })
-  const mainCategories: MainCategories = await client.get({ endpoint: 'maincategory' })
-  const subCategories: SubCategories = await client.get({ endpoint: 'subcategory' })
-
-  //Json作成
-  const category: Category[] = []
-  mainCategories.contents.map((mc) => {
-    const SC: SubCategory[] = []
-    subCategories.contents.map((sc) => {
-      if (mc.id == sc.mainCategory.id) SC.push({ id: sc.id, category: sc.subCategory })
-    })
-    category.push({ id: mc.id, category: mc.category, subCategories: SC })
-  })
 
   return {
     props: {
       blog: blog,
-      categories: category,
     },
   }
 }
