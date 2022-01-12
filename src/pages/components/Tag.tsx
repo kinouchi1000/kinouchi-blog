a8c627070b221a72c5df0f4a858bed9f6e75b5c8
kinouchi1000 <drums.radwimps203@gmail.com>
2022-01-12 20:53:22 +0900

    fix : console.logの削除

--- src/pages/components/Tag.tsx
+++ src/pages/components/Tag.tsx
@@ -5,7 +5,6 @@ interface Props {
   tagName?: string
 }
 export const Tag: React.FC<Props> = ({ tagName }) => {
-  console.log('debug')
   return (
     <>
       <div className='px-5 m-2 w-fit text-indigo-50 bg-indigo-600 rounded-xl shadow-sm'>
